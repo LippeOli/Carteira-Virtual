cdca8a1d87adf81e8875acb13cbcf2e63f18340b
LippeOli <lipe060672@gmail.com>
2024-11-03 23:22:38 -0300

    update: despesas tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { ChartPrimeiro } from "./components/ChartPrimeiro";
+import Despesas from './components/Despesas';
 
 function App() {
+
   return (
-    <div>
+    <div className="flex flex-col">
       <h1 className='text-purple-500'>Olaaa</h1>
-      <ChartPrimeiro></ChartPrimeiro>
+      <br />
+      <Despesas />
+      <div className="flex justify-center mt-16">
+        <ChartPrimeiro />
+      </div>
     </div>
   );
 }
