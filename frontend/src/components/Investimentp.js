bb8edef3ef83a6abdce61663f0dd80694486d6cc
LippeOli <lipe060672@gmail.com>
2025-01-06 14:49:10 -0300

    add: selecao mes investimento

--- frontend/src/components/Investimentp.js
+++ frontend/src/components/Investimentp.js
@@ -1,28 +1,46 @@
 
-let mensal = 150; 
-let tempo = 16;   
+let mensal = 600; 
+let tempo = 6;   
+let selecionar = false;
+let selecionados = [1, 3, 4];
 
-function Investimento(mensal, tempo) {
+
+function Investimento(mensal, tempo, selecionar, selecionados) {
     let total = 0;
     let investido = 0;
     let lucro = 0;
     let lucroAnt = 0;
 
+
     for (let i = 1; i <= tempo; i++) {
+        
         total = (total + mensal) * (1 + 0.0093); // 0.93% de taxa de juros (0.0093)
         investido += mensal
         lucro = total - investido 
         
         let lucroMes = lucro - lucroAnt
         lucroAnt =  lucro
-
-        console.log(`\n`)
-        console.log(`Mês: ${i}`);
-        console.log(`Final: R$ ${total.toFixed(2)}`);
-        console.log(`Investido: R$ ${investido.toFixed(2)}`);
-        console.log(`Lucro: R$ ${lucro.toFixed(2)}`);
-        console.log(`Lucro Mes: R$ ${lucroMes.toFixed(2)}`);
+        
+        if(selecionar){
+            
+            if(selecionados.includes(i)){
+                console.log(`\n`)
+                console.log(`Mês: ${i}`);
+                console.log(`Final: R$ ${total.toFixed(2)}`);
+                console.log(`Investido: R$ ${investido.toFixed(2)}`);
+                console.log(`Lucro: R$ ${lucro.toFixed(2)}`);
+                console.log(`Lucro Mes: R$ ${lucroMes.toFixed(2)}`);        
+            }
+        }else{
+            console.log(`\n`)
+            console.log(`Mês: ${i}`);
+            console.log(`Final: R$ ${total.toFixed(2)}`);
+            console.log(`Investido: R$ ${investido.toFixed(2)}`);
+            console.log(`Lucro: R$ ${lucro.toFixed(2)}`);
+            console.log(`Lucro Mes: R$ ${lucroMes.toFixed(2)}`);
+    
+        }
     }
 }
 
-Investimento(mensal, tempo);
+Investimento(mensal, tempo, selecionar, selecionados);
