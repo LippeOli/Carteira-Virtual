9a76855edbb94c9e94369d7441b2924ae9c6a9b8
LippeOli <lipe060672@gmail.com>
2025-03-07 21:20:25 -0300

    fix: margins dashboard

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -21,8 +21,8 @@ function Dashboard() {
   }, [recarregarGrafico]);
 
   return (
-    <div className="bg-stone-950 h-screen text-white flex flex-col justify-center items-center">
-      <div className="bg-slate-950 h-64 w-9/12 rounded-xl border-2 border-slate-600">
+    <div className="bg-stone-950 h-auto text-white flex flex-col justify-center">
+      <div className="bg-slate-950 h-64 w-5/12 ml-12 mt-8 rounded-xl border-2 border-slate-600">
         <div className="bg-slate-900 rounded-t-xl h-14 flex justify-between border-b-2 border-slate-600">
           <h1 className="ml-4 mt-3 font-mono text-lg">Adicione sua despesa</h1>
           <button className="mr-6 mt-2 bg-slate-400 w-10 h-10 rounded-xl">Info</button>
@@ -34,7 +34,7 @@ function Dashboard() {
         </div>
       </div>
 
-      <div className="bg-slate-950 w-9/12 mt-12 rounded-xl border-2 border-slate-600">
+      <div className="bg-slate-950 w-5/12 ml-12 mt-8 mb-24 rounded-xl border-2 border-slate-600">
         <div className="mt-8 ml-4 mb-4 flex gap-8">
           {/* Passar a função de inicialização para o gráfico */}
           <GrafDesp onInitialize={handleGraficoInit} />
